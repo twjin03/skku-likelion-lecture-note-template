df3bbcf3ad1fe8ba4784bcd168e52d1392a3db5a
twjin03 <twjindev@gmail.com>
2025-04-10 20:22:26 +0900

    3rd_session

--- src/notes/list/Note2.js
+++ src/notes/list/Note2.js
@@ -1,37 +1,22 @@
-// import React, { useState } from "react";
-
-// export default function Note2() {
-//   const [val, setVal] = useState('test');
-
-//   return (
-//     <div>
-//       <h1>hello</h1>
-//       {val}
-//       <button onClick={() => setVal('테스트')}>
-//         변경
-//       </button>
-//     </div>
-//   );
-// }
-
-
 import React, { useState } from "react";
 import style from '../css/ApiPractice.module.css';
 
 export default function ApiPractice() {
   const [userId, setUserId] = useState(1);
-  const [user, setUser] = useState(''); 
+  const [user, setUser] = useState('');
   const [postTitle, setPostTitle] = useState('');
   const [post, setPost] = useState('');
 
   const [error, setError] = useState('');
+  const [response, setResponse] = useState('');
 
   const handleGetUser = () => {
     console.log("userId:", userId);
     fetch("https://jsonplaceholder.typicode.com/users/" + userId)
-      .then((res) =>{
-        if(!res.ok) throw new Error('no user')
-        return res.json();})
+      .then((res) => {
+        if (!res.ok) throw new Error('no user')
+        return res.json();
+      })
       .then((data) => {
         setUser(data);
         console.log("fetched user:", data);
@@ -41,6 +26,37 @@ export default function ApiPractice() {
         console.error("fetch error:", error);
       });
   };
+  const handlePostBlog = () => {
+    console.log("userId:", userId);
+    fetch("https://jsonplaceholder.typicode.com/posts/", {
+      method: "post",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify(
+        {
+          title: postTitle,
+          body: post,
+          userId: 1
+        }
+      )
+    })
+      .then((res) => {
+        // if(!res.ok) throw new Error('no user')
+        return res.json();
+      })
+      .then((data) => {
+        // setUser(data);
+        // console.log("fetched user:", data);
+        console.log(data);
+        setResponse(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.error("fetch error:", error);
+      });
+  };
+
 
   return (
     <div className={style.container}>
@@ -85,12 +101,15 @@ export default function ApiPractice() {
           placeholder="게시글 내용"
           rows="4"
         />
-        <button className={style.button}>
+        <button className={style.button} onClick={handlePostBlog}>
           게시글 작성하기 POST
         </button>
       </div>
+      {response && <div className={`${style.response} ${style.success}`} >
+        게시글이 생성되었습니다: {response.id}
+      </div>}
 
-      {error && <div className={style.error + " " + style.response} >
+      {error && <div className={`${style.response} ${style.success}`} >
         오류: {error}
       </div>}
     </div>
